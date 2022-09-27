b6614345f702e8568bd95ac01a72872796d859dd
Christian <cgulick@student.fitchburgstate.edu>
2022-09-26 22:36:57 -0400

    Nav mesh

--- Advance Game Workshop/Assets/Scripts/DroneMove.cs
+++ Advance Game Workshop/Assets/Scripts/DroneMove.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class DroneMove : MonoBehaviour
 {
@@ -9,25 +10,55 @@ public class DroneMove : MonoBehaviour
    
     public float speed;
     public float rSpeed = 10.0f;
+    private NavMeshAgent drone;
+    public float visDistance;
+    public int randomPP;
+    public Vector3 visionBox;
+    
    
     // Start is called before the first frame update
     void Start()
     {
-        
+        drone = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
+         
+
+          drone.destination = patrolPoints[currentPP].transform.position;
+
+          if (Vector3.Distance(transform.position, patrolPoints[currentPP].transform.position) < 2)
+          {
+                drone.destination = patrolPoints[currentPP].transform.position;
+
+               randomPP = Random.Range(0,patrolPoints.Length);
+               currentPP = randomPP;
+          }  
+
+         
+    }
+    
+   void OnDrawGizmos() 
+   {
+          // Gizmos.color = Color.red;
+
+          // Gizmos.DrawRay(transform.position, transform.forward * visDistance);
+          // Gizmos.DrawWireCube(transform.position + transform.forward * visDistance, visionBox);
+   }
+    void MoveToPoint()
+    {
+     
        if (Vector3.Distance(transform.position, patrolPoints[currentPP].transform.position) < 2)
        {
-            currentPP++;
+            
        }
 
-       if (currentPP >= patrolPoints.Length)
+      /* if (currentPP >= patrolPoints.Length)
        {
             currentPP = 0;
-       }
+       } */
        
        Quaternion lookatPP = Quaternion.LookRotation(patrolPoints[currentPP].transform.position - transform.position);
        transform.rotation = Quaternion.Slerp(transform.rotation, lookatPP, rSpeed * Time.deltaTime);
